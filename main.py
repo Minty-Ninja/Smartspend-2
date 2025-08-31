84b9894032aec018ad2f7c0831a38297e792d495
Gargi Singh <37056484-gargisingh7@users.noreply.replit.com>
2025-08-31 06:21:21 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import turtle
+import random
+t = turtle.Turtle()
+screen = turtle.Screen()
+screen.bgcolor("indigo")
+t.pensize(3)
+t.speed(40)
+
+for i in range(15):
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    t.pencolor(r,g,b)
+    t.fillcolor(r,g,b)
+    x = random.randint(-500,500)
+    y = random.randint(-500,500)
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+
+    count = 0
+    t.begin_fill()
+    while(count<6):
+     t.forward(100)
+     t.left(144)
+     count = count+1
+    t.end_fill()
+    t.right(45)
